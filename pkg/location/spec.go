32816a387036047b89889d5549c3095692ed53b7
Ronnie Ayala <ronnie.ayala@outlook.com>
2022-10-24 11:47:19 -0500

    fix functions locations

--- pkg/location/spec.go
+++ pkg/location/spec.go
@@ -2,4 +2,4 @@ package location
 
 const StandardCdc = "cmd/standardcdc/main.go"
 const AdvancedCdc = "cmd/advancedcdc/main.go"
-const EventHandler = "cmd/advancedcdc/main.go"
+const EventHandler = "cmd/eventhandler/main.go"
